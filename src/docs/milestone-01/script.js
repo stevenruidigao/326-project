b2376d9ebfd6103258e8c6c85f4e84d7f48b3b5f
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-03-29 17:32:05 -0400

    milestone-01: add JS, CSS, few sections (WIP)
    Added images for 3/4 team members.
    Added content for team, overview, application parts.
    Added JS for smooth scrolling with navbar & word counter
    Added CSS for sticky navbar & rotating profile images on hover.

---
+++ src/docs/milestone-01/script.js
@@ -0,0 +1,49 @@
+/*
+- **DOM Access**: 
+- **Dynamic DOM Updates**: 
+- **Event-Driven Trigger**: 
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contentSections = document.querySelectorAll("main section");
+
+  for (const section of contentSections) {
+    const h2 = section.querySelector("h2");
+    const content = section.querySelector("p");
+
+    h2.addEventListener("mouseover", () => {
+      const counter = document.createElement("span");
+      const length = content.innerText.split(/[\s+\.]+/).length - 1;
+
+      counter.classList.add("word-count");
+      counter.innerText = `${length} words`;
+
+      h2.append(counter);
+
+      h2.addEventListener(
+        "mouseout",
+        () => {
+          counter.remove();
+        },
+        { once: true },
+      );
+    });
+  }
+
+  // smooth scroll
+  const links = document.querySelectorAll("#navbar a");
+
+  for (const link of links) {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const section = document.querySelector(
+        `section${link.getAttribute("href")}`,
+      );
+
+      section.scrollIntoView({ behavior: "smooth" });
+
+      return false;
+    });
+  }
+});
