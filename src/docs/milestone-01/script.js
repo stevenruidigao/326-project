3883a2ad3f1a74d49361b17213b261590bca5261
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-03-30 18:29:19 -0400

    milestone-01: finish documentation & add wireframes, use dark theme

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,48 +1,41 @@
-/*
-- **DOM Access**: 
-- **Dynamic DOM Updates**: 
-- **Event-Driven Trigger**: 
-*/
-
+/* - **DOM Access**:
+- **Dynamic DOM Updates**:
+- **Event-Driven Trigger**: */
 document.addEventListener("DOMContentLoaded", () => {
   const contentSections = document.querySelectorAll("main section");
 
   for (const section of contentSections) {
     const h2 = section.querySelector("h2");
-    const content = section.querySelector("p");
+    const content = section.querySelector(".content");
 
+    if (!content) continue;
+    
     h2.addEventListener("mouseover", () => {
       const counter = document.createElement("span");
-      const length = content.innerText.split(/[\s+\.]+/).length - 1;
+      const length = content.innerText
+        .split(/\s+(?:-\s+)?/)
+        .filter(Boolean).length;
 
       counter.classList.add("word-count");
       counter.innerText = `${length} words`;
-
       h2.append(counter);
 
-      h2.addEventListener(
-        "mouseout",
-        () => {
-          counter.remove();
-        },
-        { once: true },
-      );
+      h2.addEventListener("mouseout", () => {
+        counter.remove();
+      }, { once: true });
     });
   }
-
+  
   // smooth scroll
   const links = document.querySelectorAll("#navbar a");
-
+  
   for (const link of links) {
     link.addEventListener("click", (e) => {
       e.preventDefault();
-
       const section = document.querySelector(
         `section${link.getAttribute("href")}`,
       );
-
       section.scrollIntoView({ behavior: "smooth" });
-
       return false;
     });
   }
