a1c6f614966994c714c054d6576bfa987528d0ff
Restyled.io <commits@restyled.io>
2024-04-16 20:55:42 +0000

    Restyled by clang-format

--- src/client/js/api/index.js
+++ src/client/js/api/index.js
@@ -1,28 +1,32 @@
-import * as mock from "./mock/index.js";
 import * as local from "./local.js";
+import * as mock from "./mock/index.js";
 
 /*
-    NOTE: .find() doesn't return total_rows equivalent (since we're querying data)
-    so we don't know when pagination ends until we reach a page with no rows.
-    Those queries will have to likely implement a "show more" button/infinite scrolling on the frontend
+    NOTE: .find() doesn't return total_rows equivalent (since we're querying
+   data) so we don't know when pagination ends until we reach a page with no
+   rows. Those queries will have to likely implement a "show more"
+   button/infinite scrolling on the frontend
 */
 const withPagination = (pageSize) => async (page, cb) => {
   page = Math.max(1, page);
 
   const start = (page - 1) * pageSize;
-  const res = await cb({ limit: pageSize, skip: start });
+  const res = await cb({limit : pageSize, skip : start});
 
-  if (res.warning) console.warn(`[PouchDB] ${res.warning}`);
+  if (res.warning)
+    console.warn(`[PouchDB] ${res.warning}`);
 
-  const data = { ...(res.rows || res.docs) };
+  const data = {...(res.rows || res.docs)};
   const totalPages = Math.ceil(res.total_rows / pageSize);
 
   // Not present for .find() results. See note above function
   if (totalPages) {
     data.pagination = {};
 
-    if (page > 1) data.pagination.prev = Math.min(totalPages, page - 1);
-    if (page < totalPages) data.pagination.next = page + 1;
+    if (page > 1)
+      data.pagination.prev = Math.min(totalPages, page - 1);
+    if (page < totalPages)
+      data.pagination.next = page + 1;
     data.pagination.total = totalPages;
   }
 
@@ -34,48 +38,48 @@ const withPagination = (pageSize) => async (page, cb) => {
 const APPOINTMENTS_PAGE_SIZE = 8;
 const appointmentsPagination = withPagination(APPOINTMENTS_PAGE_SIZE);
 
-const allAppointments = (page = 1) =>
-  appointmentsPagination(page, (opts) =>
-    mock.appointments.allDocs({
-      include_docs: true,
+const allAppointments = (page = 1) => appointmentsPagination(
+    page,
+    (opts) => mock.appointments.allDocs({
+      include_docs : true,
       ...opts,
     }),
-  );
+);
 
 const getAppointment = (id) => mock.appointments.get(id);
 
-const withUserAppointments = (userId, page = 1) =>
-  appointmentsPagination(page, (opts) =>
-    mock.appointments.find({
-      selector: { $or: [{ teacherId: userId }, { learnerId: userId }] },
+const withUserAppointments = (userId, page = 1) => appointmentsPagination(
+    page,
+    (opts) => mock.appointments.find({
+      selector : {$or : [ {teacherId : userId}, {learnerId : userId} ]},
       ...opts,
     }),
-  );
+);
 
-const withTeacherAppointments = (userId, page = 1) =>
-  appointmentsPagination(page, (opts) =>
-    mock.appointments.find({
-      selector: { teacherId: userId },
+const withTeacherAppointments = (userId, page = 1) => appointmentsPagination(
+    page,
+    (opts) => mock.appointments.find({
+      selector : {teacherId : userId},
       ...opts,
     }),
-  );
+);
 
-const withLearnerAppointments = (userId, page = 1) =>
-  appointmentsPagination(page, (opts) =>
-    mock.appointments.find({
-      selector: { learnerId: userId },
+const withLearnerAppointments = (userId, page = 1) => appointmentsPagination(
+    page,
+    (opts) => mock.appointments.find({
+      selector : {learnerId : userId},
       ...opts,
     }),
-  );
+);
 
 // modify
-// -> default attrs probably? but those would happen in backend anyways (validation & stuff)
-const createAppointment = (data) =>
-  mock.appointments.post({
-    ...data,
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-  });
+// -> default attrs probably? but those would happen in backend anyways
+// (validation & stuff)
+const createAppointment = (data) => mock.appointments.post({
+  ...data,
+  createdAt : Date.now(),
+  updatedAt : Date.now(),
+});
 
 const updateAppointment = async (id, data) => {
   const doc = await mock.appointments.get(id);
@@ -86,23 +90,23 @@ const updateAppointment = async (id, data) => {
   return mock.appointments.put({
     ...doc,
     ...data,
-    _id: id,
-    _rev: doc._rev,
-    updatedAt: Date.now(),
+    _id : id,
+    _rev : doc._rev,
+    updatedAt : Date.now(),
   });
 };
 
 export const appointments = {
   // fetch
-  all: allAppointments,
-  get: getAppointment,
-  withUser: withUserAppointments,
-  withTeacher: withTeacherAppointments,
-  withLearner: withLearnerAppointments,
+  all : allAppointments,
+  get : getAppointment,
+  withUser : withUserAppointments,
+  withTeacher : withTeacherAppointments,
+  withLearner : withLearnerAppointments,
 
   // modify
-  create: createAppointment,
-  update: updateAppointment,
+  create : createAppointment,
+  update : updateAppointment,
 };
 
 // ===== USERS =====
@@ -112,40 +116,42 @@ const userPagination = withPagination(USERS_PAGE_SIZE);
 
 // TODO  hadle password in backend
 
-const registerUser = ({ name, username, email, password }) =>
-  mock.users.post({ name, username, email });
+const registerUser = ({name, username, email, password}) =>
+    mock.users.post({name, username, email});
 
 const getUser = (id) => mock.users.get(id);
 
-const allUsers = (page = 1) =>
-  userPagination(page, (opts) =>
-    mock.users.allDocs({
-      include_docs: true,
+const allUsers = (page = 1) => userPagination(
+    page,
+    (opts) => mock.users.allDocs({
+      include_docs : true,
       ...opts,
     }),
-  );
+);
 
 // Get users that have ANY of the skills listed AND any of the skills wanted
 const allUsersWithSkills = (page = 1, skillsHad = [], skillsWant = []) =>
-  userPagination(page, (opts) =>
-    mock.users.find({
-      selector: {
-        $and: [
-          skillsHad.length && {
-            $or: skillsHad.map((skill) => ({
-              skills: { $elemMatch: { $eq: skill } },
-            })),
+    userPagination(
+        page,
+        (opts) => mock.users.find({
+          selector : {
+            $and : [
+              skillsHad.length && {
+                $or : skillsHad.map((skill) => ({
+                                      skills : {$elemMatch : {$eq : skill}},
+                                    })),
+              },
+              skillsWant.length && {
+                $or : skillsWant.map(
+                    (skill) => ({
+                      skillsWanted : {$elemMatch : {$eq : skill}},
+                    })),
+              },
+            ].filter(Boolean),
           },
-          skillsWant.length && {
-            $or: skillsWant.map((skill) => ({
-              skillsWanted: { $elemMatch: { $eq: skill } },
-            })),
-          },
-        ].filter(Boolean),
-      },
-      ...opts,
-    }),
-  );
+          ...opts,
+        }),
+    );
 
 const updateUser = async (id, data) => {
   const doc = await mock.users.get(id);
@@ -156,18 +162,18 @@ const updateUser = async (id, data) => {
   return mock.users.put({
     ...doc,
     ...data,
-    _id: id,
-    _rev: doc._rev,
-    updatedAt: Date.now(),
+    _id : id,
+    _rev : doc._rev,
+    updatedAt : Date.now(),
   });
 };
 
 export const users = {
-  register: registerUser,
-  get: getUser,
-  all: allUsers,
-  withSkills: allUsersWithSkills,
-  update: updateUser,
+  register : registerUser,
+  get : getUser,
+  all : allUsers,
+  withSkills : allUsersWithSkills,
+  update : updateUser,
 };
 
 // ===== SESSION =====
@@ -184,11 +190,15 @@ const getSession = async () => {
 const getSessionUser = async () => {
   const data = await getSession();
 
-  if (!data?.userId) return null;
+  if (!data?.userId)
+    return null;
 
   // user id does not exist.
   // TODO handle error somehow?
   return users.get(data.userId);
 };
 
-export const session = { get: getSession, getUser: getSessionUser };
+export const session = {
+  get : getSession,
+  getUser : getSessionUser
+};
