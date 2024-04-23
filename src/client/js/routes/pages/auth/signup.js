304c912cc29498be507bc8a4aa97af22225a119b
Restyled.io <commits@restyled.io>
2024-04-23 18:06:17 +0000

    Restyled by clang-format

--- src/client/js/routes/pages/auth/signup.js
+++ src/client/js/routes/pages/auth/signup.js
@@ -1,12 +1,13 @@
-import { session, users } from "../../../api/index.js";
-import { setupNavbar } from "../../../layout.js";
-import { app, toggleElement } from "../../helper.js";
-import { goToRoute } from "../../index.js";
+import {session, users} from "../../../api/index.js";
+import {setupNavbar} from "../../../layout.js";
+import {app, toggleElement} from "../../helper.js";
+import {goToRoute} from "../../index.js";
 
 let errorsEl = null;
 
 export const showErrors = (errors) => {
-  if (!errorsEl) console.warn("`errorsEl` has not been initialized!");
+  if (!errorsEl)
+    console.warn("`errorsEl` has not been initialized!");
   else {
     errorsEl.innerText = errors.join("\n");
 
@@ -26,7 +27,7 @@ export const register = async (data) => {
   } catch (err) {
     console.error("An error occurred during registration --", err);
 
-    showErrors([err]);
+    showErrors([ err ]);
 
     return;
   }
@@ -35,8 +36,8 @@ export const register = async (data) => {
     await session.create(id);
   } catch (err) {
     console.error(
-      "An error occurred logging you in. User was created! --",
-      err,
+        "An error occurred logging you in. User was created! --",
+        err,
     );
 
     // redirect to login page
@@ -53,8 +54,8 @@ export default (args, doc) => {
   const formContainer = doc.querySelector("#signup-form");
   const form = doc.querySelector("form");
   const submitButton = form.querySelector("[type=submit]");
-  const requiredFields = [...form.querySelectorAll("input[required]")].map(
-    (el) => el.name,
+  const requiredFields = [...form.querySelectorAll("input[required]") ].map(
+      (el) => el.name,
   );
 
   errorsEl = doc.querySelector("#signup-form-errors");
@@ -68,18 +69,17 @@ export default (args, doc) => {
     const errors = [];
 
     for (const key of requiredFields) {
-      if (data.get(key)?.trim()) continue;
+      if (data.get(key)?.trim())
+        continue;
 
       errors.push(`The ${key} is required.`);
     }
 
-    const next = errors.length
-      ? Promise.resolve(showErrors(errors))
-      : register(Object.fromEntries(data.entries()));
+    const next = errors.length ? Promise.resolve(showErrors(errors))
+                               : register(Object.fromEntries(data.entries()));
 
-    next
-      .then(() => setupNavbar())
-      .finally(() => submitButton.classList.remove("is-loading"));
+    next.then(() => setupNavbar())
+        .finally(() => submitButton.classList.remove("is-loading"));
   });
 
   app.appendChild(formContainer);
