15ff039980171fb9f0a56f95e1b521f4d9e2f28c
Steven Gao <stevenruidigao@gmail.com>
2024-04-23 15:48:14 -0400

    switch to params

--- src/client/js/routes/pages/browse.js
+++ src/client/js/routes/pages/browse.js
@@ -1,15 +1,32 @@
 import { app } from "../helper.js";
-import { HTMLAppRouteElement, goToRoute } from "../index.js";
+import { HTMLAppRouteElement, goToRoute, getCurrent } from "../index.js";
 import * as api from "../../api/index.js";
 
 export const onunload = async (prev, next) => {
   console.log(`[browse] unloading ${prev.file} for ${next.file}!`);
 };
 
-async function renderUsers(page, skillsHad, skillsWant) {
+function generateSearchParams() {
+  const hasInput = document.getElementById("search-has");
+  const wantsInput = document.getElementById("search-wants");
+
+  const params = new URLSearchParams();
+  params.set("has", hasInput.value.split(/,\s*/g).filter((str) => str !== "").join(","));
+  params.set("wants", wantsInput.value.split(/,\s*/g).filter((str) => str !== "").join(","));
+
+  return params;
+}
+
+/**
+ * Renders a list of users with their profile pictures, names, usernames, and skills into the "browse" HTML element.
+ *
+ * @param {number} page - The page number to fetch users from.
+ * @param {Array<string>} skillsHad - The skills that the users to search for had.
+ * @param {Array<string>} skillsWant - The skills that the users to search for want.
+ */
+async function renderUsers(page=1, skillsHad=[], skillsWant=[]) {
   const browseEl = document.getElementById("browse");
   const users = Array.from(await api.users.withSkills(page, skillsHad, skillsWant));
-  browseEl.innerHTML = "";
 
   for (const user of users) {
     const userEl = document.createElement("browse-user");
@@ -24,42 +41,48 @@ async function renderUsers(page, skillsHad, skillsWant) {
     nameEl.innerText = user.name;
     nameEl.className = "name";
 
-    // const usernameEl = document.createElement("app-route");
-    // const usernameEl = document.createElement("a");
-    // usernameEl.setAttribute("name", "profile");
-    // usernameEl.target = "_blank";
-
     const usernameEl = new HTMLAppRouteElement();
-    usernameEl.setAttribute("is", "app-route");
     usernameEl.setAttribute("slot", "username");
     usernameEl.setAttribute("route", "profile");
     usernameEl.setAttribute(":id", user._id);
     usernameEl.innerText = `@${user.username}`;
     usernameEl.className = "username";
 
-    const skillsEl = document.createElement("span");
-    skillsEl.setAttribute("slot", "skills");
-    skillsEl.className = "skills";
+    const hasSkillsEl = document.createElement("span");
+    hasSkillsEl.setAttribute("slot", "hasSkills");
+    hasSkillsEl.className = "skills";
+
+    hasSkillsEl.innerText = "Has skills: " + (user.skills.length == 0 ? "None" : "");
 
     for (const skill of user.skills) {
-      // const skillEl = document.createElement("app-route");
-      // const skillEl = document.createElement("a");
-      // skillEl.setAttribute("name", "browse");
-      // skillEl.target = "_blank";
-
       const skillEl = new HTMLAppRouteElement();
-      skillEl.setAttribute("is", "app-route");
-      skillEl.setAttribute("route", "search");
-      skillEl.setAttribute(":search", skill);
+      skillEl.setAttribute("route", "browse");
+      skillEl.setAttribute("search", "?has=" + skill);
       skillEl.innerText = skill;
       skillEl.className = "skill";
-      skillsEl.appendChild(skillEl);
+      hasSkillsEl.appendChild(skillEl);
+    }
+
+    const wantsSkillsEl = document.createElement("span");
+    wantsSkillsEl.setAttribute("slot", "wantsSkills");
+    wantsSkillsEl.className = "skills";
+
+    wantsSkillsEl.innerText = "Wants skills: " + (user.skillsWanted.length == 0 ? "None" : "");
+
+    for (const skill of user.skillsWanted) {
+      const skillEl = new HTMLAppRouteElement();
+      skillEl.setAttribute("route", "browse");
+      skillEl.setAttribute("search", "?wants=" + skill);
+      skillEl.innerText = skill;
+      skillEl.className = "skill";
+      wantsSkillsEl.appendChild(skillEl);
     }
 
     userEl.appendChild(profilePictureEl);
     userEl.appendChild(nameEl);
     userEl.appendChild(usernameEl);
-    userEl.appendChild(skillsEl);
+    userEl.appendChild(hasSkillsEl);
+    userEl.appendChild(wantsSkillsEl);
 
     browseEl.appendChild(userEl);
   }
@@ -73,18 +96,25 @@ export default async (args) => {
   const searchDiv = document.createElement("div");
   searchDiv.id = "search-div";
 
-  const input = document.createElement("input");
-  input.type = "text";
-  input.placeholder = "Search";
-  input.className = "input is-light";
-  input.id = "search-box";
+  const hasInput = document.createElement("input");
+  hasInput.type = "text";
+  hasInput.placeholder = "Skills you want";
+  hasInput.className = "input is-light";
+  hasInput.id = "search-has";
+
+  const wantsInput = document.createElement("input");
+  wantsInput.type = "text";
+  wantsInput.placeholder = "Skills you have";
+  wantsInput.className = "input is-light";
+  wantsInput.id = "search-wants";
 
   const button = document.createElement("button");
   button.innerText = "Search";
   button.className = "button is-light";
   button.id = "search-button";
 
-  searchDiv.appendChild(input);
+  searchDiv.appendChild(hasInput);
+  searchDiv.appendChild(wantsInput);
   searchDiv.appendChild(button);
 
   app.appendChild(searchDiv);
@@ -95,30 +125,43 @@ export default async (args) => {
   app.appendChild(browseEl);
 
   if (args.search) {
-    input.value = args.search;
-    renderUsers(0, args.search.split(/,\s*/g).filter((str) => str !== ""), []);
-    
-  } else {
-    renderUsers(0, [], []);
+    hasInput.value = args.search;
   }
 
-  input.addEventListener("input", () => {
-    renderUsers(0, input.value.split(/,\s*/g).filter((str) => str !== ""), []);
+  const params = getCurrent() ? getCurrent().search : null;
+
+  if (params) {
+    hasInput.value = params.get("has");
+    wantsInput.value = params.get("wants");
+  }
+
+  browseEl.innerHTML = "";
+  renderUsers(0, hasInput.value.split(/,\s*/g).filter((str) => str !== ""), wantsInput.value.split(/,\s*/g).filter((str) => str !== ""));
+
+  // Add event listeners for searching users
+  hasInput.addEventListener("input", () => {
+    browseEl.innerHTML = "";
+    renderUsers(0, hasInput.value.split(/,\s*/g).filter((str) => str !== ""), wantsInput.value.split(/,\s*/g).filter((str) => str !== ""));
   });
 
-  input.addEventListener("keypress", (event) => {
+  wantsInput.addEventListener("input", () => {
+    browseEl.innerHTML = "";
+    renderUsers(0, hasInput.value.split(/,\s*/g).filter((str) => str !== ""), wantsInput.value.split(/,\s*/g).filter((str) => str !== ""));
+  });
+
+  hasInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-      goToRoute("search", {
-        search: input.value.split(/,\s*/g).filter((str) => str !== "").join(","),
-      });
+      goToRoute("browse", {}, generateSearchParams());
+    }
+  });
+
+  wantsInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      goToRoute("browse", {}, generateSearchParams());
     }
   });
 
   button.addEventListener("click", () => {
-    goToRoute("search", {
-      search: input.value,
-    });
-
-    // renderUsers(0, input.value.split(", ").filter((str) => str !== ""), []);
+    goToRoute("browse", {}, generateSearchParams());
   });
 };
