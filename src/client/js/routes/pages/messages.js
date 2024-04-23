eb4a709d540d0c48c20adf88afd895529995621b
David Zhan <zhandavidz@gmail.com>
2024-04-23 15:42:16 -0400

    add message managing logic + basic rendering stuff

--- src/client/js/routes/pages/messages.js
+++ src/client/js/routes/pages/messages.js
@@ -17,7 +17,7 @@ export const onunload = async (prev, next) => {
 };
 
 
-export default async (args) => {
+export default async (args, doc) => {
   app.innerHTML = "";
 
   console.log("** messages loaded with args", args);
@@ -32,17 +32,61 @@ export default async (args) => {
     return routes.goToRoute("home");
   }
 
-  // TODO: figure out why I need an Array.from here
-  const allUserMessages = Array.from(await api.messages.allWithUser(user._id));
   
+  const allUserMessages = (await api.messages.allWithUser(user._id)).docs;
+  
+  // group messages by user
+  const conversations = allUserMessages.reduce((acc, msg) => {
+    const otherUserId = msg.fromId === user._id ? msg.toId : msg.fromId;
+    if (!acc[otherUserId]) {
+      acc[otherUserId] = [];
+    }
+    acc[otherUserId].push(msg);
+    return acc;
+  }, {});
+
+  // in-place sort conversations by most recent message
+  for (const convoKey in conversations) {
+    conversations[convoKey].sort((a, b) => b.time - a.time);
+  }
+
+  // sort the keys of conversations by most recent message to display most recent conversations at top
+  const orderOfConversations = Object.keys(conversations).sort((a, b) => {
+    const lastMsgA = conversations[a][0];
+    const lastMsgB = conversations[b][0];
+    return lastMsgB.time - lastMsgA.time;
+  });
+
+  console.log("conversations", conversations);
+  console.log("orderOfConversations", orderOfConversations);
+
   // TODO: render the sidebar with all the user's conversations + msg previews + set all to not highlighted (maybe check if class exists, and if its there remove it)
+  // make sure these conversation previews are links to goToRoute("conversation", { id: conversationId })
+  const sidebarEl = doc.getElementById("message-sidebar").cloneNode(true);
+  app.appendChild(sidebarEl);
+
+  const previewContainer = document.getElementById("message-list");
+  
+  for (const convoKey of orderOfConversations) {
+    const otherUserId = convoKey;
+    const otherUser = await api.users.get(otherUserId);
+    const lastMsg = conversations[convoKey][0];
+
+    // FIXME: how to set the args?
+    const convoEl = new HTMLAppRouteElement();
+    convoEl.route = "conversation";
+    convoEl.arg = otherUserId;
+    // convoEl.className = "conversation";
+    convoEl.innerText = `${otherUser.username}: ${lastMsg.text}`;
+    previewContainer.appendChild(convoEl);
+  }
   
   try {
     // clean up the id of other user (treats garbage id as undefined)
     const otherUserId = cleanId(args.id);
-
+    
     // check to see if the other user exists, if doesn't error, continue rendering
-    api.users.get(otherUserId);
+    await api.users.get(otherUserId);
     
     // TODO: render the full conversation specified
 
