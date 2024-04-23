064a4e212f9e4772e32b8aa901344a414c22e699
Wenhao <wenhaoyang2015@gmail.com>
2024-04-23 17:08:28 -0400

    Make login functional

--- src/client/js/routes/pages/login.js
+++ src/client/js/routes/pages/login.js
@@ -1,5 +1,17 @@
 import { app } from "../helper.js";
 import { goToRoute } from "../index.js";
+import { session, users } from "../../api/index.js";
+import { setupNavbar } from "../../layout.js";
+import { toggleElement } from "../helper.js";
+import { showErrors } from "./auth/signup.js";
+
+const showError = (el, error) => {
+  if (!el) return console.warn("error display element not found"); // Nowhere to display
+  else {
+    el.innerText = error
+    toggleElement(el, "is-hidden", !error);
+  }
+};
 
 export default (args, doc) => {
   app.innerHTML = "";
@@ -12,18 +24,24 @@ export default (args, doc) => {
   // doc is DocumentFragment of corresponding HTML file
   // can querySelector, etc... to add to app
   
-  const login = doc.querySelector("#login").cloneNode(true);
-  
-  // TODO: Make button functional!!!
+  const login = doc.querySelector("#login"); // Don't need to use cloneNode?
+  const form = doc.querySelector("form");
+  const button = doc.querySelector("#login-submit");
+  const error = doc.querySelector("#login-error");
 
-  /*submit.addEventListener("click", () => {
-    console.log("login button clicked");
-    // TODO: Display loading screen while waiting
-    // TODO: Communicate to server
-    // Create a session? 
-    goToRoute("home"); // Temporary
-    // Remove this event listener before it's deleted?
-  });*/
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    button.classList.add("is-loading");
+    
+    const data = new FormData(form);
+
+    users.login({ username: data.get("username"), password: data.get("password") })
+      .then(user => {
+        session.create(user._id).then(_ => goToRoute("dashboard").then(_ => setupNavbar()));
+      })
+      .catch(err => {console.error(err), showError(error, err)})
+      .finally(() => button.classList.remove("is-loading"));
+  });
 
   app.appendChild(login);
 
