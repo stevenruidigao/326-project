39a3a1fc0b087ce4fa4355933b2e4ffbe3d1f443
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-04-24 18:13:38 -0400

    jsdoc: comment api/index.js

--- src/client/js/api/index.js
+++ src/client/js/api/index.js
@@ -1,12 +1,28 @@
 import * as local from "./local.js";
 import * as mock from "./mock/index.js";
 
-/*
-    NOTE: .find() doesn't return total_rows equivalent (since we're querying
-   data) so we don't know when pagination ends until we reach a page with no
-   rows. Those queries will have to likely implement a "show more"
-   button/infinite scrolling on the frontend
-*/
+/**
+ * @typedef {T[] & { pagination?: { prev?: number, next?: number, total?: number }}} PaginatedArray<T>
+ * @template {any} T
+ */
+
+/**
+ * @typedef {{ id: string, rev: string, ok: boolean }} PouchDBResponse
+ * @typedef {{ limit: number, skip: number, attachments: boolean, binary: boolean }} PouchDBOptions
+ */
+
+/**
+ * Pagination helper function for PouchDB queries. The callback function
+ * should utilize the `opts` object to pass to the query function.
+ *
+ * NOTE: .find() doesn't return total_rows equivalent (since we're querying
+ * data) so we don't know when pagination ends until we reach a page with no
+ * rows. Those queries will have to likely implement a "show more"
+ * button/infinite scrolling on the frontend
+ * @param {number} pageSize
+ * @returns {(page: number, cb: (opts: { limit: number, skip: number }) => Promise<any>) =>
+ *  Promise<PaginatedArray>}
+ */
 const withPagination = (pageSize) => async (page, cb) => {
   page = Math.max(1, page);
 
@@ -37,6 +53,11 @@ const withPagination = (pageSize) => async (page, cb) => {
 const APPOINTMENTS_PAGE_SIZE = 8;
 const appointmentsPagination = withPagination(APPOINTMENTS_PAGE_SIZE);
 
+/**
+ *
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<Appointment>>}
+ */
 const allAppointments = (page = 1) =>
   appointmentsPagination(page, (opts) =>
     mock.appointments.allDocs({
@@ -45,8 +66,20 @@ const allAppointments = (page = 1) =>
     }),
   );
 
+/**
+ * Obtain a specific appointment by ID
+ * @param {string} id
+ * @returns {Promise<Appointment>}
+ */
 const getAppointment = (id) => mock.appointments.get(id);
 
+/**
+ * Obtain all appointments involving a specific user.
+ * NOTE: Paginated, not sorted!
+ * @param {string} userId
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<Appointment>>}
+ */
 const withUserAppointments = (userId, page = 1) =>
   appointmentsPagination(page, (opts) =>
     mock.appointments.find({
@@ -55,6 +88,13 @@ const withUserAppointments = (userId, page = 1) =>
     }),
   );
 
+/**
+ * Obtain all appointments with a specific user as teacher.
+ * NOTE: Paginated, not sorted!
+ * @param {string} userId
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<Appointment>>}
+ */
 const withTeacherAppointments = (userId, page = 1) =>
   appointmentsPagination(page, (opts) =>
     mock.appointments.find({
@@ -63,6 +103,13 @@ const withTeacherAppointments = (userId, page = 1) =>
     }),
   );
 
+/**
+ * Obtain all appointments with a specific user as learner.
+ * NOTE: Paginated, not sorted!
+ * @param {string} userId
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<Appointment>>}
+ */
 const withLearnerAppointments = (userId, page = 1) =>
   appointmentsPagination(page, (opts) =>
     mock.appointments.find({
@@ -92,6 +139,12 @@ const getAppointmentUsersInvolved = async (appts) => {
 // modify
 // -> default attrs probably? but those would happen in backend anyways
 // (validation & stuff)
+
+/**
+ * Create a new appointment
+ * @param {Appointment} data
+ * @returns {Promise<PouchDBResponse>}
+ */
 const createAppointment = (data) =>
   mock.appointments.post({
     ...data,
@@ -99,6 +152,12 @@ const createAppointment = (data) =>
     updatedAt: Date.now(),
   });
 
+/**
+ * Update an appointment's data
+ * @param {string} id
+ * @param {Appointment} data
+ * @returns {Promise<PouchDBResponse>}
+ */
 const updateAppointment = async (id, data) => {
   const doc = await mock.appointments.get(id);
 
@@ -133,8 +192,17 @@ export const appointments = {
 const MESSAGES_PAGE_SIZE = 10;
 const messagesPagination = withPagination(MESSAGES_PAGE_SIZE);
 
+/**
+ * Get all messages in the database
+ * @returns {Promise<PaginatedArray<Message>>}
+ */
 const getAllMessages = () => mock.messages.allDocs({ include_docs: true });
 
+/**
+ * Get all messages involving a specific user
+ * @param {string} userId
+ * @returns {Promise<{ docs: Message[] }>}
+ */
 const getAllMessagesInvolvingUser = (userId) =>
   mock.messages.find({
     selector: {
@@ -143,7 +211,13 @@ const getAllMessagesInvolvingUser = (userId) =>
     // sort: ["time"],
   });
 
-// FIXME: pagination does not correctly give newest messages first
+/**
+ * Get messages involving a specific user, paginated.
+ * FIXME: pagination does not correctly give newest messages first
+ * @param {string} userId
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<Message>>}
+ */
 const getMessagesInvolvingUser = (userId, page = 1) => {
   console.warn("pagination does not correctly give newest messages first");
   messagesPagination(page, (opts) =>
@@ -158,6 +232,12 @@ const getMessagesInvolvingUser = (userId, page = 1) => {
   );
 };
 
+/**
+ * Create new message.
+ * NOTE: Should not include & not return ID!
+ * @param {Message} data
+ * @returns {Promise<Message>}
+ */
 const createMessage = (data) => {
   const newMsg = {
     ...data,
@@ -166,10 +246,6 @@ const createMessage = (data) => {
   mock.messages.post(newMsg);
   return newMsg;
 };
-// mock.messages.post({
-//   ...data,
-//   time: Date.now(),
-// });
 
 // TODO: should I add functions to get all messages?
 export const messages = {
@@ -189,6 +265,11 @@ const userPagination = withPagination(USERS_PAGE_SIZE);
 
 // TODO  handle password in backend
 
+/**
+ * Obtain user given credentials. Throws error if no user found.
+ * @param {{ username: string, password: string }} args
+ * @returns {Promise<User>}
+ */
 const loginUser = async ({ username, password }) => {
   const results = await mock.users.find({
     selector: {
@@ -203,6 +284,11 @@ const loginUser = async ({ username, password }) => {
   return results.docs[0];
 };
 
+/**
+ * Register user given credentials. Throws error if user already exists with email or username.
+ * @param {{ name: string, username: string, email: string, password: string }} param0
+ * @returns {Promise<PouchDBResponse>}
+ */
 const registerUser = async ({ name, username, email, password }) => {
   const [emailOut, usernameOut] = await Promise.all([
     mock.users.find({
@@ -227,8 +313,20 @@ const registerUser = async ({ name, username, email, password }) => {
   return mock.users.post({ name, username, email });
 };
 
+/**
+ * Obtain user by ID
+ * @param {string} id
+ * @param {PouchDBOptions} opts
+ * @returns {Promise<User>}
+ */
 const getUser = (id, opts = {}) => mock.users.get(id, opts);
 
+/**
+ * Obtain user by username
+ * @param {string} username
+ * @param {PouchDBOptions} opts
+ * @returns
+ */
 const getUserByUsername = (username, opts) =>
   mock.users
     .find({
@@ -238,6 +336,11 @@ const getUserByUsername = (username, opts) =>
     })
     .then((out) => out.docs[0]);
 
+/**
+ * Obtain user's avatar image
+ * @param {User} user
+ * @returns {Promise<Blob>}
+ */
 const getUserAvatar = async (user) => {
   const avatar = user._attachments?.avatar;
 
@@ -251,6 +354,11 @@ const getUserAvatar = async (user) => {
   return attachment;
 };
 
+/**
+ * Paginate through all users
+ * @param {number} page
+ * @returns {Promise<PaginatedArray<User>>}
+ */
 const allUsers = (page = 1) =>
   userPagination(page, (opts) =>
     mock.users.allDocs({
@@ -259,7 +367,15 @@ const allUsers = (page = 1) =>
     }),
   );
 
-// Get users that have ANY of the skills listed AND any of the skills wanted
+/**
+ * Get users that have ANY of the skills listed AND any of the skills wanted.
+ * NOTE: uses pagination, but behind the scenes it fetches ALL users and filters each time,
+ * since this query does not allow for an index to be used.
+ * @param {number} page
+ * @param {string[]} skillsHad
+ * @param {string[]} skillsWant
+ * @returns
+ */
 const allUsersWithSkills = (page = 1, skillsHad = [], skillsWant = []) =>
   userPagination(page, (opts) =>
     mock.users.find({
@@ -318,13 +434,28 @@ export const users = {
 // session user data to prevent multiple fetches
 let currentUser = undefined;
 
-export const setSessionCurrent = (u) => (currentUser = u);
-export const getSessionCurrent = async () => {
+/**
+ * Store the current session user
+ * @param {User} u
+ * @returns {User}
+ */
+const setSessionCurrent = (u) => (currentUser = u);
+
+/**
+ * Obtain the current session user. Fetches if not already stored.
+ * @returns {Promise<User?>}
+ */
+const getSessionCurrent = async () => {
   if (currentUser !== undefined) return currentUser;
   return setSessionCurrent(await getSessionUser());
 };
 
-export const createSession = async (userId) => {
+/**
+ * MOCK ONLY: Creates session for user id (i.e. logged in state)
+ * @param {number} userId
+ * @returns {Promise<void>}
+ */
+const createSession = async (userId) => {
   const doc = await mock.session.post({ userId });
 
   console.log("createSession", doc);
@@ -334,7 +465,11 @@ export const createSession = async (userId) => {
   setSessionCurrent();
 };
 
-export const getSession = async () => {
+/**
+ * Get current session data (if exists), uses token stored
+ * @returns {Promise<Session?>}
+ */
+const getSession = async () => {
   const id = await local.get("token");
 
   if (id) {
@@ -346,6 +481,10 @@ export const getSession = async () => {
   return null;
 };
 
+/**
+ * Get current logged-in user from session data
+ * @returns {Promise<User?>}
+ */
 const getSessionUser = async () => {
   const session = await getSession();
 
@@ -357,7 +496,11 @@ const getSessionUser = async () => {
     .catch(() => null);
 };
 
-export const deleteSession = async () => {
+/**
+ * Delete current session (i.e. log out)
+ * @returns {Promise<void>}
+ */
+const deleteSession = async () => {
   const session = await getSession();
 
   if (session) await mock.session.remove(session);
