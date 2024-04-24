483d9963169554f1002bb51aafc8d82b21ad7ad5
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-04-23 20:39:53 -0400

    auth: slight touchup on login/signup html

--- src/client/js/routes/pages/auth/login.js
+++ src/client/js/routes/pages/auth/login.js
@@ -16,12 +16,6 @@ export default (args, doc) => {
   app.innerHTML = "";
 
   console.log("** login loaded with args", args);
-
-  // args is key-value object of arguments pased to path
-  // e.g. /profile/:id obtains { id: VALUE }
-
-  // doc is DocumentFragment of corresponding HTML file
-  // can querySelector, etc... to add to app
   
   const login = doc.querySelector("#login"); // Don't need to use cloneNode?
   const form = doc.querySelector("form");
