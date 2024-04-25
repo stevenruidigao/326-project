eefdd1f1efa772f258f1e13f0d4bbfb904639b93
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-04-24 23:25:48 -0400

    jsdoc dayjs

--- src/client/js/dayjs.js
+++ src/client/js/dayjs.js
@@ -3,11 +3,30 @@ import "./libs/dayjs-relativeTime.js";
 
 dayjs.extend(window.dayjs_plugin_relativeTime);
 
+/**
+ * Format a time in the format "YYYY-MM-DD HH:mm".
+ * @param time dayjs resolvable
+ * @returns {string}
+ */
 export const formatTime = (time) => dayjs(time).format("YYYY-MM-DD HH:mm");
 
+/**
+ * Format a time in the format "MMMM D, YYYY at h:mm A" - e.g. "January 1, 2022 at 12:00 PM".
+ * @param time dayjs resolvable
+ * @returns {string}
+ */
 export const formatTimeVerbose = (time) =>
   dayjs(time).format("MMMM D, YYYY [at] h:mm A");
 
+/**
+ * Get how long ago a time was ("2 hours ago") or how far in the future ("in 2 hours")
+ * @param time dayjs resolvable
+ * @returns {string}
+ */
 export const formatRelative = (time) => dayjs(time).fromNow();
 
+/**
+ * The dayjs library, also available in the window scope.
+ * Extended with the relativeTime plugin.
+ */
 export default dayjs;
